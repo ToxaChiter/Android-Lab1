05b12d4451e8fcadea0ba9088bc4092a23b7977f
Anton Lobarev <loborew@gmail.com>
2024-03-15 22:44:51 +0300

    Добавьте файлы проекта.

---
+++ Lab1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab1;
+
+using Microsoft.Maui.Controls.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    async void OnMapClicked(object sender, MapClickedEventArgs e)
+    {
+        await DisplayAlert("Lobarev says", $"Latitude: {e.Location.Latitude}, Longitude: {e.Location.Longitude}", "OK");
+    }
+}
